cc9f4f19a0f1d0b3fe496bd1e511ed7bb5e150b4
Jerry <260902108@qq.com>
2023-05-10 02:37:51 +0800

    5.10

--- pages/dashboard/dashboard.vue
+++ pages/dashboard/dashboard.vue
@@ -41,15 +41,15 @@
 				isCustom color="#FFF" title="">
 				<!-- #ifndef MP-ALIPAY || MP-BAIDU -->
 				<template>
-					<view class="tabs">
-						<tui-tab :leftGap="40" :size="40" backgroundColor="#FFFFFF000" selectedColor="#000"
+					<tui-wing-blank size="large">
+						<tui-tab :leftGap="30" :size="40" backgroundColor="#FFFFFF000" selectedColor="#000"
 							sliderBgColor="#27AE60" bold :tabs="tabs" scroll @change="change"></tui-tab>
-					</view>
+					</tui-wing-blank>
 				</template>
 				<!-- #endif -->
 			</tui-navigation-bar>
 			<view :style="{marginTop:`calc(${navigationBarHeight}px + 20rpx)`}">
-				<view class="tui-header-slide relative">
+				<tui-wing-blank size="large" class="relative">
 					<swiper class="swiper" circular :indicator-dots="true" indicator-color="#FFF"
 						indicator-active-color="#07C160" :autoplay="true">
 						<swiper-item v-for="(banner,index) in banners" :key="index">
@@ -58,10 +58,23 @@
 							</view>
 						</swiper-item>
 					</swiper>
-				</view>
-				<tui-wing-blank size="large">
-					<xui-card>
-						Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis, tempore nam. Ut ab, veritatis iusto fuga mollitia perspiciatis ea natus corrupti ipsa quo ducimus reprehenderit, tempore tempora modi id sint corporis laborum qui?
+					<xui-card :hover="false">
+						<view class="flex items-center justify-between">
+							<view class="flex items-center">
+								<text>2023年</text>
+								<text>05月</text>
+								<tui-icon name="calendar"></tui-icon>
+							</view>
+							<view class="flex items-center">
+								<text>明细</text>
+								<tui-icon name="arrowright"></tui-icon>
+							</view>
+						</view>
+						<view class="">
+							<tui-charts-line ref="tui_line_1" tooltip :xAxis="options1.xAxis" :yAxisLine="options1.yAxisLine" :yAxisLabel="options1.yAxisLabel"
+								:dataset="options1.dataset" :max="options1.max" :splitLine="options1.splitLine" :splitNumber="options1.splitNumber"
+								@click="dotClick"></tui-charts-line>
+						</view>
 					</xui-card>
 				</tui-wing-blank>
 			</view>
@@ -96,6 +109,22 @@
 						url: '../../static/imgs/banner4.png'
 					}
 				],
+				options1: {
+					xAxis: ['一月', '二月', '三月', '四月', '五月', '六月'],
+					dataset: [{
+						name: '营业额',
+						color: '#5677fc',
+						source: [380, 210, 0, 160, 30, 0],
+					}],
+					max: 600,
+					splitNumber: 100,
+					splitLine: {
+						color: "transparent",
+						type: "dashed"
+					},
+					yAxisLine:{color: 'transparent',itemGap: 60},
+					yAxisLabel:	{show: false,color: "#333",size: 24},
+				},
 			}
 		},
 		computed: {
@@ -116,6 +145,10 @@
 					title: ''
 				})
 			}
+		},
+		//绘制图表
+		onReady() {
+			this.$refs.tui_line_1 && (this.$refs.tui_line_1.draw(this.options1.dataset))
 		},
 		methods: {
 			...mapMutations(["changeTabBar", "resetTabBarIndex"]),
@@ -165,6 +198,12 @@
 					}
 				})
 			},
+			dotClick() {
+
+			},
+			change(){
+				
+			}
 		}
 	}
 </script>
@@ -232,31 +271,23 @@
 		width: 80%;
 	}
 
-	.tabs {
-		margin-left: 40rpx;
-	}
+	swiper {
+		width: 688rpx;
+		height: 246rpx;
+		margin: 0 auto;
+		border-radius: 48rpx;
+		overflow: hidden;
 
-	.tui-header-slide {
-		z-index: 999;
-		margin: 0 4%;
+		.swiper-item {
+			height: 100%;
+			overflow: hidden;
 
-		swiper {
-			width: 688rpx;
-			height: 246rpx;
-			margin: 0 auto;
-			border-radius: 48rpx;
-			overflow: hidden;;
-			.swiper-item {
+			/* fallback for old browsers */
+			// /* Chrome 10-25, Safari 5.1-6 */
+			// /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+			image {
+				width: 100%;
 				height: 100%;
-				overflow: hidden;
-
-				/* fallback for old browsers */
-				// /* Chrome 10-25, Safari 5.1-6 */
-				// /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-				image {
-					width: 100%;
-					height: 100%;
-				}
 			}
 		}
 	}
